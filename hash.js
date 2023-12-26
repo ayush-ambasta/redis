9a81e1debf1f9724de78b9d3f1728db0c740403d
Ayush Ambasta <ayushambasta2001@gmail.com>
2023-12-26 13:42:16 +0530

    hash in redis

---
+++ hash.js
@@ -0,0 +1,23 @@
+const client=require('./client');
+
+async function init(){
+    const hash=await client.hset(
+        'student:1',
+        {
+            name:'Ayush',
+            batch:'2020',
+            branch:'ECE',
+            Degree:'Btech'
+        }
+    )
+
+    console.log(`Number of fields were added: ${hash}`);
+
+    const name = await client.hget('student:1', 'name');
+    console.log(`name: ${name}`);
+
+    const student = await client.hgetall('student:1');
+    console.log(student);
+}
+
+init();
